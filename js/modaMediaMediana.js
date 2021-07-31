0a84d7960ca310a28a1b26aff27079e935c7efd8
Miguel Ponce Local <miguel_p1194@hotmail.com>
2021-07-31 18:26:58 -0500

    Se agrega la funcion para calcular la moda

--- js/modaMediaMediana.js
+++ js/modaMediaMediana.js
@@ -30,3 +30,20 @@ function calcularMediana(lista){
         return mediana= lista[mitadLista];
     }
 }
+
+// CALCULAR MODA
+function calcularModa(lista){
+    const listaContar = {};
+    lista.map(function(elemento){
+        if (listaContar[elemento]){
+            listaContar[elemento] +=1;
+        } else {
+            listaContar[elemento]=1;
+        }
+    });
+    const listaArreglo = Object.entries(listaContar).sort((elementoA, elementoB)=>{
+        return elementoA[1] - elementoB[1];
+    });
+    
+    return moda = listaArreglo[listaArreglo.length -1];
+}
