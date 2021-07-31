35337d46800191f83ed1747e2b781d8c9c93222e
Miguel Ponce Local <miguel_p1194@hotmail.com>
2021-07-30 20:45:15 -0500

    Se agrega la funcionalidad de la calculadora de porcentajes y descuentos

--- js/descuentos.js
+++ js/descuentos.js
@@ -4,3 +4,43 @@ function calcularDescuento(precio, descuento){
     
     return precioConDescuento;    
 }
+
+let cupones= ["bienvenido", "hotsale", "platzi"];
+
+function calcularPrecio(){
+    let precio= parseInt(document.getElementById("precio").value);
+    let descuento = parseInt(document.getElementById("descuento").value);
+    let resultado = document.getElementById("resultado");
+    let cupon  = (document.getElementById("cupon").value).toLowerCase() ;
+    let precioDescuento;
+    let cuponValor;
+
+    if (isNaN(precio) || isNaN(descuento)){
+        alert("Los campos Precio y Descuento no deben quedar vacios");
+    } else{
+        if (cupon === ""){
+            precioDescuento = calcularDescuento(precio, descuento);
+            resultado.innerText= "$ "+precioDescuento; 
+        } else if (cupon === cupones[0]){
+            cuponValor = 5;
+            descuento += cuponValor; 
+            precioDescuento = calcularDescuento(precio, descuento);
+            resultado.innerText= "$ "+precioDescuento; 
+            alert("Tu cupon es del 5%");   
+        } else if (cupon === cupones[1]){
+            cuponValor= 15;
+            descuento += cuponValor;
+            precioDescuento = calcularDescuento(precio, descuento);
+            resultado.innerText= "$ "+precioDescuento; 
+            alert("Tu cupon es del 15%");   
+        } else if (cupon == cupones[2]){
+            cuponValor= 25;
+            descuento += cuponValor;
+            precioDescuento = calcularDescuento(precio, descuento);
+            resultado.innerText= "$ "+precioDescuento; 
+            alert("Tu cupon es del 25%");   
+        }else{
+            alert(`El cupon ${cupon} NO es valido`);
+        }
+    }    
+}
